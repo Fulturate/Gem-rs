c4669dfc9856633cd068916f9ed32122dddb9777
nixxoq <nixxoq@outlook.com>
2025-08-30 15:28:27 +0300

    oopsie

--- src/types.rs
+++ src/types.rs
@@ -153,6 +153,7 @@ pub struct Part {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Blob {
     mime_type: String,
     data: String, // Base64 encoded data
